b943a3ad740f3479aaac44e1a50090e681c70401
devlllkk02 <73985687+devlllkk02@users.noreply.github.com>
2021-07-02 22:14:33 +0400

    Added developmentkeys.js

---
+++ config/developmentKeys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MONGO_URI:
+    "mongodb+srv://admin:LvKxcqawjPYWWRkM@cluster0.7oeta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  JWT_SECRET: "1hby26348hufHydh",
+  SENDGRID_API:
+    "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",
+  EMAIL: "http://localhost:3000",
+};
